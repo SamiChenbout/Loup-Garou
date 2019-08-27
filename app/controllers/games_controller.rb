13abb89fe34215e5616d59da69f16bb22bca6a8b
Hugo MANCINI <hgmancini@gmail.com>
2019-08-27 11:31:58 +0200

    games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -15,18 +15,17 @@ class GamesController < ApplicationController
 
   def find_game
 
-    characters = Character.all
+    @characters = Character.all
     if Game.where(step: "waiting") != []
       @game = Game.where(step: "waiting").first
       roles = @game.characters if @game.characters != nil
-      characters -= roles
-      @player = Player.new(user: current_user, game: @game)
+      @characters -= roles if @game.characters != nil
+      @player = Player.new(user: current_user, game: @game, character: @characters.sample)
     else
       @game = Game.new
-      @player = Player.new(user: current_user, game: @game)
+      @player = Player.new(user: current_user, game: @game, character: @characters.sample)
     end
       @game.save
-      @player.character = characters.sample
       @player.save
       redirect_to game_path(@game)
   end
