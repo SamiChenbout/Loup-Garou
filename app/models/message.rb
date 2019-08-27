c99383a133085fd74841052348634c1616566881
Hubert Kuoch <hubert.kuoch@gmail.com>
2019-08-27 15:33:16 +0200

    Adding Chatroom to Game show

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,20 @@
 class Message < ApplicationRecord
+  after_create :broadcast_message
+
   belongs_to :player
   belongs_to :game
 
   validates :content, presence: true
+
+  private
+
+  def broadcast_message
+    ActionCable.server.broadcast("game_#{game.id}", {
+      message_partial: ApplicationController.renderer.render(
+        partial: "messages/show",
+        locals: { message: self } # user_is_messages_author: false
+      ),
+      current_user_id: self.player.user.id
+    })
+  end
 end
