e74cfea9426995e4498b61dced34c38bb4ca6fa8
Hubert Kuoch <hubert.kuoch@gmail.com>
2019-08-28 17:14:16 +0200

    sequence

--- app/javascript/packs/night.js
+++ app/javascript/packs/night.js
@@ -4,58 +4,70 @@ const firstNight = () => {
     document.querySelector('.cupidon').classList.remove('d-none');
     randomCouple();
     // Voyante
-    document.querySelector('.cupidon').classList.add('d-none');
-    document.querySelector('.voyante').classList.remove('d-none');
-    voyante();
-    // loup
-    document.querySelector('.voyante').classList.add('d-none');
-    document.querySelector('.loup').classList.remove('d-none');
-    loup();
-    // sorciere
-    document.querySelector('.loup').classList.add('d-none');
-    document.querySelector('.sorciere').classList.remove('d-none');
-    sorciere();
-    // le jour se leve !
-    document.querySelector('.sorciere').classList.add('d-none');
+    setTimeout(function() {
+      document.querySelector('.cupidon').classList.add('d-none');
+      document.querySelector('.voyante').classList.remove('d-none');
+      voyante();
+    }, 3000)
+    // // loup
+    setTimeout(function() {
+      document.querySelector('.voyante').classList.add('d-none');
+      document.querySelector('.loup').classList.remove('d-none');
+      loup();
+    }, 6000)
+    // // sorciere
+    setTimeout(function() {
+      document.querySelector('.loup').classList.add('d-none');
+      document.querySelector('.sorciere').classList.remove('d-none');
+      sorciere();
+    }, 9000)
+    // // le jour se leve !
+    // document.querySelector('.sorciere').classList.add('d-none');
   };
 }
 
-
 // Accessing game id
-const gameDiv = document.getElementById("connect-to-game-channel");
-if (gameDiv) {
-  const gameId = gameDiv.dataset.gameId;
-}
 
 // Default action calls if no action taken by user
 const randomCouple = () => {
+  const gameDiv = document.getElementById("connect-to-game-channel");
+  const gameId = gameDiv.dataset.gameId;
   setTimeout(function() {
     fetch(`/games/${gameId}/random_couple`);
-  }, 30000);
+  }, 3000);
 }
 
 const voyante = () => {
+  const gameDiv = document.getElementById("connect-to-game-channel");
+  const gameId = gameDiv.dataset.gameId;
   setTimeout(function() {
     fetch(`/games/${gameId}/voyante_next_step`);
-  }, 30000);
+  }, 3000);
 }
 
 const loup = () => {
+  const gameDiv = document.getElementById("connect-to-game-channel");
+  const gameId = gameDiv.dataset.gameId;
   setTimeout(function() {
     fetch(`/games/${gameId}/random_loup_vote`);
-  }, 30000);
+  }, 3000);
 }
 
 const sorciere = () => {
+  const gameDiv = document.getElementById("connect-to-game-channel");
+  const gameId = gameDiv.dataset.gameId;
   setTimeout(function() {
+    console.log("rien");
     fetch(`/games/${gameId}/random_sorciere_vote`);
-  }, 30000);
+  }, 3000);
 }
 
 const chasseur = () => {
+  const gameDiv = document.getElementById("connect-to-game-channel");
+  const gameId = gameDiv.dataset.gameId;
   setTimeout(function() {
     fetch(`/games/${game_id}/chasseur_random_kill`);
-  }, 30000);
+  }, 3000);
 }
 
 export { firstNight };
