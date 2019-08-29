25d89aa3a5ef11d0d9a6b5ef3ebbfa697b530753
Hubert Kuoch <hubert.kuoch@gmail.com>
2019-08-29 16:31:08 +0200

    Wolf tchit tchat

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,12 @@
 import "bootstrap";
 import { mycheck } from "./checkbox";
 import { initActionCable } from "../plugins/init_action_cable";
+import { initActionCableLoup } from "../plugins/init_action_cable_loup";
 import { timer } from "../plugins/timer";
 import { cupidon } from "./night"
 
 mycheck();
 initActionCable();
+initActionCableLoup();
 cupidon();
 timer();
--- app/javascript/plugins/init_action_cable.js
+++ app/javascript/plugins/init_action_cable.js
@@ -1,20 +1,21 @@
 const initActionCable = () => {
-  // Calling the ad hoc game div defined in game show
   const gameDiv = document.getElementById("connect-to-game-channel");
   if (gameDiv) {
-    // Getting game id back from the ad hoc game div defined in game show
     const gameId = gameDiv.dataset.gameId;
-    // Subscribing game players/users into same cable
     App[`game_${gameId}`] = App.cable.subscriptions.create(
       { channel: 'GamesChannel', game_id: gameId },
       { received: (data) => {
-        // Avoiding to show user sent message twice (because of AJAX)
         console.log(data);
-        document.getElementById("number-players").innerText = data.number_of_players;
-        const currentUserId = parseInt(gameDiv.dataset.currentUserId, 10);
-        if (data.current_user_id !== currentUserId) {
-          document.getElementById("messages").insertAdjacentHTML('beforeend', data.message_partial);
+        const numberPlayerSpan = document.getElementById("number-players");
+        if (numberPlayerSpan) {
+          document.getElementById("number-players").innerText = data.number_of_players;
         }
+        const currentUserId = parseInt(gameDiv.dataset.currentUserId, 10);
+        const messageDiv = document.getElementById("messages");
+        if (messageDiv) {
+          if (data.current_user_id !== currentUserId) {
+          messageDiv.insertAdjacentHTML('beforeend', data.message_partial);
+        }}
       }}
     )
     App[`game_status_${gameId}`] = App.cable.subscriptions.create(
@@ -25,23 +26,23 @@ const initActionCable = () => {
 }
 
 const redirectAllPlayers = (data) => {
-  if (data.game_status.step === "starting") {
-    if (data.game_status.round_step === "cupidon" && data.game_status.round === 1) {
-      window.location = `/games/${data.game_id}/cupidon`;
-    } else if (data.game_status.round_step === "voyante") {
-      window.location = `/games/${data.game_id}/voyante`;
-    } else if (data.game_status.round_step === "loup") {
-      window.location = `/games/${data.game_id}/loup`;
-    } else if (data.game_status.round_step === "sorciere" && data.game_status.round === 1) {
-      window.location = `/games/${data.game_id}/sorciere`;
-    } else if (data.game_status.round_step === "day") {
-      window.location = `/games/${data.game_id}`;
-    } else if (data.game_status.round_step === "start-day") {
-      window.location = `/games/${data.game_id}/start-day`;
-    } else if (data.game_status.round_step === "end-day") {
-      window.location = `/games/${data.game_id}/end-day-calcul`;
-    }
-  }
+ if (data.game_status.step === "starting") {
+   if (data.game_status.round_step === "cupidon" && data.game_status.round === 1) {
+     window.location = `/games/${data.game_id}/cupidon`;
+   } else if (data.game_status.round_step === "voyante") {
+     window.location = `/games/${data.game_id}/voyante`;
+   } else if (data.game_status.round_step === "loup") {
+     window.location = `/games/${data.game_id}/loup`;
+   } else if (data.game_status.round_step === "sorciere" && data.game_status.round === 1) {
+     window.location = `/games/${data.game_id}/sorciere`;
+   } else if (data.game_status.round_step === "day") {
+     window.location = `/games/${data.game_id}`;
+   } else if (data.game_status.round_step === "start-day") {
+     window.location = `/games/${data.game_id}/start-day`;
+   } else if (data.game_status.round_step === "end-day") {
+     window.location = `/games/${data.game_id}/end-day-calcul`;
+   }
+ }
 }
 
 export { initActionCable }
