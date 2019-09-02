d99386bb73b1b4a320343c8199cbd4181a63fd85
maya douadi <maya.douadi@outlook.fr>
2019-09-02 17:09:56 +0200

    Style

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -43,7 +43,7 @@ class GamesController < ApplicationController
 
     current_user.update(points: current_user.points + @gamer.points) if current_user.points != nil
     current_user.update(points: @gamer.points) unless current_user.points != nil
-    current_user.update(level: current_user.level + 1, points: current_user.points / 1000) if current_user.points / 1000 > 1
+    current_user.update(level: current_user.level + 1, points: current_user.points / 1000) if current_user.points != nil && current_user.points / 1000 > 1
   end
 
   def role
@@ -56,5 +56,31 @@ class GamesController < ApplicationController
       @all_except_me << player if player.user != current_user && player.is_alive
       @dead << player if player.user != current_user && player.is_alive == false
     end
+  end
+
+  def day
+    @game = Game.find(params[:game_id])
+    @gamer = Player.where(user: current_user, game: @game).first
+    all = @game.players
+    @all_except_me = []
+    @dead = []
+    all.each do |player|
+      @all_except_me << player if player.user != current_user && player.is_alive
+      @dead << player if player.user != current_user && player.is_alive == false
+    end
+    @voyante = Player.where(game: @game, character: Character.where(name: "voyante").first).first
+  end
+
+  def night
+    @game = Game.find(params[:game_id])
+    @gamer = Player.where(user: current_user, game: @game).first
+    all = @game.players
+    @all_except_me = []
+    @dead = []
+    all.each do |player|
+      @all_except_me << player if player.user != current_user && player.is_alive
+      @dead << player if player.user != current_user && player.is_alive == false
+    end
+    @voyante = Player.where(game: @game, character: Character.where(name: "voyante").first).first
   end
 end
--- app/javascript/packs/night.js
+++ app/javascript/packs/night.js
@@ -1,18 +1,18 @@
 const cupidon = () => {
   if(document.querySelector('.cupidon')) {
-    myTimer(5, `random_couple_choose`);
+    myTimer(15, `random_couple_choose`);
   }
 }
 
 const voyante = () => {
   if(document.querySelector('.voyante')) {
-    myTimer(5, `voyante_next_step`);
+    myTimer(15, `voyante_next_step`);
   }
 }
 
 const loup = () => {
   if(document.querySelector('.loup')) {
-    myTimer(30000000000000, `random_loup_choose`);
+    myTimer(30, `random_loup_choose`);
   }
 }
 
@@ -28,19 +28,64 @@ const chasseur = () => {
   }
 }
 
+const night = () => {
+  if(document.querySelector('.nuit-interlude')) {
+    const gameDiv = document.getElementById("connect-to-game-channel");
+    const gameId = gameDiv.dataset.gameId;
+    const timerDiv = document.getElementById("countdown");
+    let timerLeft = 10;
+    var timer = setInterval(function(){
+      if (timerLeft) {
+        timerDiv.innerHTML = timerLeft;
+        timerLeft -= 1;
+        if(timerLeft <= 0){
+          clearInterval(timer);
+          if(gameDiv.dataset.round === "1") {
+            window.location = `/games/${gameId}/cupidon`;
+          } else if(gameDiv.dataset.voyante === "true") {
+            window.location = `/games/${gameId}/voyante`;
+          } else {
+            window.location = `/games/${gameId}/loup`;
+          }
+        }
+      }
+    }, 1000);
+  }
+}
+
+const day = () => {
+  if(document.querySelector('.day-interlude')) {
+    const gameDiv = document.getElementById("connect-to-game-channel");
+    const gameId = gameDiv.dataset.gameId;
+    const timerDiv = document.getElementById("countdown");
+    let timerLeft = 10;
+    var timer = setInterval(function(){
+      if (timerLeft) {
+        timerDiv.innerHTML = timerLeft;
+        timerLeft -= 1;
+        if(timerLeft <= 0){
+          clearInterval(timer);
+          window.location = `/games/${gameId}/when_day_comes`;
+        }
+      }
+    }, 1000);
+  }
+}
+
 const myTimer = (timeLeft, whatFetch) => {
   const gameDiv = document.getElementById("connect-to-game-channel");
   const gameId = gameDiv.dataset.gameId;
   const timerDiv = document.getElementById("countdown");
   var timer = setInterval(function(){
-    if (timerDiv)
+    if (timerDiv) {
       timerDiv.innerHTML = timeLeft;
       timeLeft -= 1;
       if(timeLeft <= 0){
         clearInterval(timer);
         fetch(`/games/${gameId}/${whatFetch}`);
       }
+    }
   }, 1000);
 }
 
-export { cupidon, voyante, chasseur, loup, sorciere };
+export { cupidon, voyante, chasseur, loup, sorciere, night, day };
